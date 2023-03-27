7251734b7840386223395954d1206dc038ca7ea5
NorrisAkg <93124855+NorrisAkg@users.noreply.github.com>
2023-03-27 18:39:50 +0100

    install vuetify

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,16 @@ import { createApp } from "vue";
 import App from "./root/App.vue";
 import { router } from "./root/router/routes";
 
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+const vuetify = createVuetify({
+    components,
+    directives,
+});
+
 const app = createApp(App);
 
-app.use(router).mount("#root");
+app.use(router).use(vuetify).mount("#root");
